4d5aa7c1513c92d475873c144d1ae229780e177b
brandtware <82943022+brandtware@users.noreply.github.com>
2025-10-16 20:32:53 +0200

    movement
    still not working properly

--- tom.gd
+++ tom.gd
@@ -1,31 +1,45 @@
 extends CharacterBody3D
 
-var move_vector : Vector3 = Vector3.FORWARD
+var move_vector : Vector3 = Vector3.ZERO
+var last_direction : Vector3 = Vector3.FORWARD
 var directionChangeTimer : float = 0.0
 var collisionCount : int = 0
 
 func _ready() -> void:
-	pass
+	Globals.move.connect(move)
+	
+func move (direction: Globals.movement):
+	#last_direction = move_vector
+	#move_vector = direction
+	print (Globals.movement.keys()[direction])
+	match direction:
+		Globals.movement.LEFT:
+			$".".rotate_y(deg_to_rad(-90))
+			move_vector = move_vector.rotated(Vector3(0.0,1.0,0.0), deg_to_rad(-90))
+		Globals.movement.RIGHT:
+			$".".rotate_y(deg_to_rad(90))
+			move_vector = move_vector.rotated(Vector3(0.0,1.0,0.0), deg_to_rad(90))
+		Globals.movement.STOP:
+			last_direction = move_vector
+			move_vector = Vector3.ZERO
+		Globals.movement.MOVE:
+			move_vector = last_direction
+			
+	directionChangeTimer = 0.0
+	print ("direction", move_vector)
+	print ("last direction", last_direction)
+	
 	
 func _process(delta: float) -> void:
 	directionChangeTimer += delta
 	var did_collide = move_and_collide(move_vector * delta)
 	if (did_collide != null):
 		collisionCount +=1
+		print ("direction before collision ", move_vector)
+		last_direction = move_vector
+		move_vector = Vector3.ZERO
 		if directionChangeTimer < 1000:
 			# TODO say something to complain
+			print ("ouch")
 			directionChangeTimer = 0
-		match (randi() %4):
-			0: 
-				move_vector = Vector3.FORWARD
-				#$tomMesh.rotation = Vector3.FORWARD
-			1: 
-				move_vector = Vector3.BACK
-				#$tomMesh.rotation = Vector3.BACK
-			2: 
-				move_vector = Vector3.LEFT
-				#$tomMesh.rotation= Vector3.LEFT
-			3: 
-				move_vector = Vector3.RIGHT
-				#$tomMesh.rotation = Vector3.RIGHT
 		
--- watchroom.gd
+++ watchroom.gd
@@ -1,4 +1,11 @@
 extends Node3D
 
 func _ready() -> void:
+	Dialogic.signal_event.connect(dialogicSignal)
 	Dialogic.start("0_intro")
+
+func dialogicSignal (arg:String):
+	if arg == "show_ui":
+		$TV_UI.visible = true
+		$Camera3D.position = Vector3 (2.024, 2.905, 1.199)
+		$Camera3D.rotation = Vector3.ZERO
